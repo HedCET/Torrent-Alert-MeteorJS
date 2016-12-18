cd4a7788fc9fd2f60827fabebcc24bf530f71446
Linto Cheeran <linto.cet@gmail.com>
2016-12-18 16:06:14 +0530

    spinner & toast added

--- client/main.js
+++ client/main.js
@@ -1,1 +1,5 @@
 import '../imports/client';
+
+document.addEventListener("WebComponentsReady", function() {
+  document.querySelector('#polymer_spinner').toggle();
+});
--- imports/client/local_components/layout-inbox/layout-user.js
+++ imports/client/local_components/layout-inbox/layout-user.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+
 Polymer({
 
   _google() {
@@ -6,28 +9,36 @@ Polymer({
         // signOut
       } else {
         if (Meteor.isCordova) {
+          document.querySelector('#polymer_spinner').toggle();
+
           Meteor.cordova_g_plus({
             cordova_g_plus: true,
             profile: ['email', 'email_verified', 'family_name', 'gender', 'given_name', 'locale', 'name', 'picture'],
             webClientId: '731987698101-thavlbcphk9v1kco7l7bl3q70dph819m.apps.googleusercontent.com',
           }, function(error) {
+            document.querySelector('#polymer_spinner').toggle();
+
             if (error) {
-              alert(error);
+              document.querySelector('#polymer_spinner').toast(error);
             }
           });
         } else {
+          document.querySelector('#polymer_spinner').toggle();
+
           Meteor.loginWithGoogle({
             requestOfflineToken: true,
             requestPermissions: ['email', 'profile']
           }, function(error) {
+            document.querySelector('#polymer_spinner').toggle();
+
             if (error) {
-              console.log('googleSignIn ErrorNo', Accounts.LoginCancelledError.numericError);
+              document.querySelector('#polymer_spinner').toast('Google SignIn ErrorNo - ' + Accounts.LoginCancelledError.numericError);
             }
           });
         }
       }
     } else {
-      console.log('lost server connection');
+      document.querySelector('#polymer_spinner').toast('lost server connection');
     }
   },
 
