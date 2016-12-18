5acc7839e170689f22108327ada3e0d004210b02
Linto Cheeran <linto.cet@gmail.com>
2016-12-18 11:27:06 +0530

    SignIn fix

--- client/main.js
+++ client/main.js
@@ -1,13 +1,1 @@
 import '../imports/client';
-
-if (Meteor.isCordova) {
-  Meteor.cordova_g_plus({
-    cordova_g_plus: true,
-    profile: ["email", "email_verified", "family_name", "gender", "given_name", "locale", "name", "picture"],
-    webClientId: '731987698101-thavlbcphk9v1kco7l7bl3q70dph819m.apps.googleusercontent.com',
-  }, function(error) {
-    if (error) {
-      alert(error);
-    }
-  });
-}
--- imports/client/local_components/layout-inbox/layout-user.js
+++ imports/client/local_components/layout-inbox/layout-user.js
@@ -1,5 +1,32 @@
 Polymer({
 
-  is: "layout-user",
+  _google_sign_in() {
+    if (Meteor.status().connected) {
+      if (Meteor.isCordova) {
+        Meteor.cordova_g_plus({
+          cordova_g_plus: true,
+          profile: ['email', 'email_verified', 'family_name', 'gender', 'given_name', 'locale', 'name', 'picture'],
+          webClientId: '731987698101-thavlbcphk9v1kco7l7bl3q70dph819m.apps.googleusercontent.com',
+        }, function(error) {
+          if (error) {
+            alert(error);
+          }
+        });
+      } else {
+        Meteor.loginWithGoogle({
+          requestOfflineToken: true,
+          requestPermissions: ['email', 'profile']
+        }, function(error) {
+          if (error) {
+            console.log('Google SignIn ErrorNo - ' + Accounts.LoginCancelledError.numericError);
+          }
+        });
+      }
+    } else {
+      console.log('lost server connection');
+    }
+  },
+
+  is: 'layout-user',
 
 });
