02fcfef12f28decaddae479dfbec6350690721a1
Linto Cheeran <linto.cet@gmail.com>
2016-12-20 02:21:38 +0530

    url added

--- server/methods/worker.js
+++ server/methods/worker.js
@@ -73,6 +73,164 @@ Meteor.methods({
         });
         break;
 
+      case 'project':
+        let project = _project.findOne({
+          query: input.query,
+        });
+
+        if (project) {
+          if (input.recent) {
+            input.project = [];
+
+            input.recent.forEach((keyword) => {
+              keyword = keyword.replace(/ seed.*?[0-9]+ ?/gi, ' ').replace(/ added.*?[0-9]+[a-z] ?/gi, ' ').replace(/\s+/g, ' ').trim();
+
+              let query = '/search?f=' + keyword + ' seed > 0 added:60d';
+
+              let project = _project.findOne({
+                query: query,
+              });
+
+              if (project) {
+                input.project.push(project._id);
+              } else {
+                input.project.push(_project.insert({ query: query, title: keyword }));
+              }
+            });
+
+            let worker = _worker.findOne({
+              _id: '_recent_',
+            });
+
+            if (worker) {
+              if (1 < moment.duration(moment().diff(worker.time)).asMinutes()) {
+                _worker.update({
+                  _id: worker._id,
+                }, {
+                  $set: {
+                    project: input.project,
+                    time: moment().toDate(),
+                  },
+                });
+              }
+            } else {
+              _worker.insert({
+                _id: '_recent_',
+                project: input.project,
+                status: '200',
+                time: moment().toDate(),
+              });
+            }
+          }
+
+          if (input.torrent) {
+            input.torrent.forEach((torrent) => {
+              let exists = _torrent.findOne({
+                query: torrent.query,
+              });
+
+              if (exists) {
+                _torrent.update({ _id: exists._id }, { $addToSet: { project: project._id }, $set: torrent });
+
+                let worker = _worker.findOne({
+                  query: torrent.query,
+                });
+
+                if (worker) {
+                  if (worker.status != '200' || 1 < moment.duration(moment().diff(worker.time)).asDays()) {
+                    _worker.update({
+                      _id: worker._id,
+                    }, {
+                      $set: {
+                        status: '',
+                      },
+                    });
+                  }
+                } else {
+                  _worker.insert({
+                    query: torrent.query,
+                    status: '',
+                    time: moment().toDate(),
+                    type: 'torrent',
+                  });
+                }
+              } else {
+                _torrent.insert(_.extend(torrent, { project: [project._id], url: [] }));
+
+                _worker.insert({
+                  query: torrent.query,
+                  status: '',
+                  time: moment().toDate(),
+                  type: 'torrent',
+                });
+              }
+            });
+          }
+
+          if (input.torrent_count) {
+            _project.update({
+              query: project._id,
+            }, {
+              $set: {
+                torrent_count: input.torrent_count,
+              },
+            });
+          }
+        } else {
+          throw new Meteor.Error(422, 'projectNotFound - { query: "' + input.query + '" }');
+        }
+
+        _worker.update({
+          _id: input._id,
+        }, {
+          $set: {
+            status: (input.error ? input.error : '200'),
+          },
+        });
+        break;
+
+      case 'torrent':
+        let torrent = _torrent.findOne({
+          query: input.query,
+        });
+
+        if (torrent) {
+          if (input.url) {
+            let _torrent_url = [];
+
+            input.url.forEach((url) => {
+              let exists = _url.findOne({
+                query: url.query,
+              })
+
+              if (exists) {
+                _torrent_url.push(exists._id);
+              } else {
+                _torrent_url.push(_url.insert(url))
+              }
+            });
+
+            _torrent.update({
+              _id: torrent._id,
+            }, {
+              $set: {
+                url: _torrent_url,
+              },
+            });
+          }
+        } else {
+          throw new Meteor.Error(422, 'torrentNotFound - { query: "' + input.query + '" }');
+        }
+
+        _worker.update({
+          _id: input._id,
+        }, {
+          $set: {
+            status: (input.error ? input.error : '200'),
+          },
+        });
+        break;
+
     }
   },
 
