a719dfbf36dbf04efec916a2970a4db3221446a6
Linto Cheeran <linto.cet@gmail.com>
2017-05-01 20:42:19 +0530

    subscription layout added

--- imports/ui/local_components/layout-user/layout-user.js
+++ imports/ui/local_components/layout-user/layout-user.js
@@ -8,6 +8,28 @@ Polymer({
     else { history.back(); }
   },
 
+  _remove() {
+    // Meteor.users.update(Meteor.user()._id, {
+    //   $pull: {
+    //     'profile.subscribed': {
+    //       $in: this.selected,
+    //     },
+    //   }
+    // });
+
+    this.selected.forEach((_id) => {
+      Meteor.users.update(Meteor.user()._id, {
+        $pull: {
+          'profile.subscribed': _id,
+        },
+      });
+    });
+
+    document.querySelector('#toast').toast(this.selected.length + ' Item Removed', 'UNDO', { redo_subscribe: this.selected });
+
+    this.set('selected', []);
+  },
+
   _SIGNOUT() {
     document.querySelector('#toast').toast('', 'SIGNOUT');
   },
@@ -32,3 +54,6 @@ Polymer({
   },
 
 });
+
+import './subscribed-item.js';
+import '../custom/selectable-icon.js';
